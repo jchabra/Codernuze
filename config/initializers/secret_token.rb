8efa29ce47028d92bd9615d086a394b978619650
Jasmine Chabra <jchabra@gmail.com>
2012-12-13 10:07:34 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Codernuze::Application.config.secret_token = '372bf6f72838fbd76add9b575c6786e8d0a178a271f916f8754ef1f953bc1643bdce20873b207bb9224767d31bb8cf5304f06f941f4cd640c38537ef33ed5762'
